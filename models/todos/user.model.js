c5e7ecaa8eea57ce565545e116b0515f4ea70537
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-24 11:34:31 +0530

    created models

---
+++ models/todos/user.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, require: true }
+}, { timestamps: true })
+
+export const User = new mongoose.model("User", userSchema);
